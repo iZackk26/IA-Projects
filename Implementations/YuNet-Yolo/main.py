d70f25e14901b0d05d3e645b1dfa66f3035a5358
izackk26 <newxstorecr@gmail.com>
2024-03-08 14:04:19 -0600

    feat: Changing color and size of image

--- Implementations/YuNet-Yolo/main.py
+++ Implementations/YuNet-Yolo/main.py
@@ -25,28 +25,41 @@ def live_face_detector():
             faces = model.infer(frame)
             for det in faces if faces is not None else []:
                 bbox = det[0:4].astype(np.int32)
-                roi = frame[bbox[1] : bbox[1] + bbox[3], bbox[0] : bbox[0] + bbox[2]]
+
+                # Aument the bounding box
+                padding_x = int(0.2 * bbox[2])
+                padding_y = int(0.2 * bbox[3])
+                x1 = max(bbox[0] - padding_x, 0)
+                y1 = max(bbox[1] - padding_y, 0)
+                x2 = min(bbox[0] + bbox[2] + padding_x, w)
+                y2 = min(bbox[1] + bbox[3] + padding_y, h)
+                roi = frame[y1:y2, x1:x2]
+                #roi = frame[bbox[1] : bbox[1] + bbox[3], bbox[0] : bbox[0] + bbox[2]]
+                cv2.imwrite("roi.jpg", roi)
                 if roi.size > 0:
                     if analize(roi) == 0:
-                        info = "Live"
+                        info = "live"
                     else:
-                        info = "Spoof"
-                cv2.rectangle(
-                    frame,
-                    (bbox[0], bbox[1]),
-                    (bbox[0] + bbox[2], bbox[1] + bbox[3]),
-                    (0, 0, 255),
-                    2,
-                )
-                cv2.putText(
-                    frame,
-                    info,
-                    (bbox[0], bbox[1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    (0, 255, 0),
-                    2,
-                )
+                        info = "spoof"
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                cv2.putText(frame, info, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+                #cv2.rectangle(
+                    #frame,
+                    #(bbox[0], bbox[1]),
+                    #(bbox[0] + bbox[2], bbox[1] + bbox[3]),
+                    #(255, 0, 0),
+                    #2,
+                #)
+                #cv2.putText(
+                    #frame,
+                    #info,
+                    #(bbox[0], bbox[1] - 10),
+                    #cv2.FONT_HERSHEY_SIMPLEX,
+                    #0.9,
+                    #(0, 255, 0),
+                    #2,
+                #)
             cv2.imshow("Video", frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
