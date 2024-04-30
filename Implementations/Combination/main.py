915013a8bedf0f76e0b4c9c057dd21f08cd2651a
izackk26 <newxstorecr@gmail.com>
2024-04-30 15:38:17 +0000

    feat: Separating all the classes

--- Implementations/Combination/main.py
+++ Implementations/Combination/main.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 from Cam.cam import Camera
 from Yunet.yunet import YuNet
-from Blinker.f_detector import eye_blink_detector
+from Blinker.blink_detection import EyeDetector
 
 info = ""
 counter = 0
@@ -23,28 +23,12 @@ def main():
         targetId=cv2.dnn.DNN_TARGET_CPU,
     )
     cam = Camera(model, 0, info) #
-    eye = eye_blink_detector()
     faces = []
     for raw_faces, frame, gray in cam.webcam():
-        img_post = frame
-        processed_faces = []
-        if raw_faces is not None:
-            for face in raw_faces:
-                x, y, w, h = face[:4]  # Tomar las primeras 4 coordenadas que son x, y, w, h
-                x1, y1 = int(x), int(y)  # Coordenada superior izquierda
-                x2, y2 = int(x + w), int(y + h)  # Coordenada inferior derecha
-                processed_face = dlib.rectangle(left=x1, top=y1, right=x2, bottom=y2)
-                processed_faces.append(processed_face)
-            boxes_face = eye.convert_rectangles2array(processed_faces, frame)
-            if len(boxes_face) > 0:
-                areas = eye.get_areas(boxes_face)
-                boxes_face = np.expand_dims(boxes_face[0], axis=0)
-                counter, total = eye.eye_blinker(processed_faces[0], gray, counter, total)
-                img_post = eye.bounding_box(frame, boxes_face, [last_detected])
-                if img_post is not None and img_post.size != 0:
-                    cv2.imshow("Blinker", img_post)
-        else:
-            img_post = frame
+        blinker = EyeDetector(raw_faces, frame, gray, counter, total)
+        img_post = blinker.detect_blink()
+        if img_post is not None and img_post.size != 0:
+            cv2.imshow("Blink Detection", img_post)
 
 
 
