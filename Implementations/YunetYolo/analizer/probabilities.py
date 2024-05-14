43d0394ce77c41ca5dcfa0e655f7d10ce305dd80
izackk26 <newxstorecr@gmail.com>
2024-05-14 14:49:40 +0000

    feat: Implementing the new YunetYolo algorithim that is separated from the others ones

---
+++ Implementations/YunetYolo/analizer/probabilities.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+# from ultralytics.engine.results import Results
+
+
+def get_probs(img, model_selected):
+    if model_selected == "ImageQuality":
+        model = YOLO("Models/ImageQuality.pt")
+    else:
+        model = YOLO("Models/3DFaceModel.pt")
+
+    results = model(img, verbose=False)
+    # return True
+    return results[-1].probs.top1
