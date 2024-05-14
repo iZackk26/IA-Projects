8068e17ce06763f2b740ab0e178880a7af1d31ae
akhoz <adrian25092005@gmail.com>
2024-05-14 08:55:16 -0600

    fix: fixing

--- Implementations/YunetYolo/analizer/probabilities.py
+++ Implementations/YunetYolo/analizer/probabilities.py
@@ -2,12 +2,8 @@ from ultralytics import YOLO
 # from ultralytics.engine.results import Results
 
 
-def get_probs(img, model_selected):
-    if model_selected == "ImageQuality":
-        model = YOLO("Models/ImageQuality.pt")
-    else:
-        model = YOLO("Models/3DFaceModel.pt")
-
+def get_probs(img):
+    model = YOLO("ImageQuality.pt")
     results = model(img, verbose=False)
     # return True
     return results[-1].probs.top1
--- Implementations/YunetYolo/main.py
+++ Implementations/YunetYolo/main.py
@@ -5,26 +5,27 @@ from yunet.yunet import YuNet
 
 info = ""
 
+
 def yunet_yolo_face_detector():
     global info
     model = YuNet(
-            modelPath="yunet/face_detection_yunet_2023mar.onnx",
-            inputSize=[320, 320],
-            confThreshold=0.9,
-            nmsThreshold=0.3,
-            topK=100,
-            backendId=cv2.dnn.DNN_BACKEND_OPENCV,
-            targetId=cv2.dnn.DNN_TARGET_CPU,
-        )
+        modelPath="yunet/face_detection_yunet_2023mar.onnx",
+        inputSize=[320, 320],
+        confThreshold=0.9,
+        nmsThreshold=0.3,
+        topK=100,
+        backendId=cv2.dnn.DNN_BACKEND_OPENCV,
+        targetId=cv2.dnn.DNN_TARGET_CPU,
+    )
 
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
         if frame is not None:
-            h,w = frame.shape
-            model.setInputSize([w,h])
+            h, w, _ = frame.shape
+            model.setInputSize([w, h])
             faces = model.infer(frame)
-            for det in (faces if faces is not None else []):
+            for det in faces if faces is not None else []:
                 bbox = det[0:4].astype(np.int32)
 
                 padding_x = int(0.2 * bbox[2])
@@ -36,23 +37,30 @@ def yunet_yolo_face_detector():
                 y2 = min(bbox[1] + bbox[3] + padding_y, h)
 
                 roi = frame[y1:y2, x1:x2]
-                roi = frame[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
+                roi = frame[bbox[1] : bbox[1] + bbox[3], bbox[0] : bbox[0] + bbox[2]]
 
                 if roi.size > 0:
-                    if get_probs(roi, model):
+                    if get_probs(roi):
                         info = "Live"
                     else:
                         info = "Spoof"
-                cv2.rectangle(frame, (x1,y1), (x2, y2), (255,255,0), 2)
-                cv2.putText(frame, info, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,0), 2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 2)
+                cv2.putText(
+                    frame,
+                    info,
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 0),
+                    2,
+                )
 
             cv2.imshow("frame", frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         cap.release()
         cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
     yunet_yolo_face_detector()
-
-
